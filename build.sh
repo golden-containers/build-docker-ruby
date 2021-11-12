27431493dce26deca9f5d368b81fb7b7d7545373
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-11 16:43:51 -0800

    Add slim

--- build.sh
+++ build.sh
@@ -15,10 +15,13 @@ cd ruby
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/" 3.0/bullseye/Dockerfile
 echo "LABEL $1" >> 3.0/bullseye/Dockerfile
 
+sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian:bullseye-slim/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/debian:bullseye-slim/" 3.0/slim-bullseye/Dockerfile
+echo "LABEL $1" >> 3.0/slim-bullseye/Dockerfile
+
 # Build
 
 docker build --tag ghcr.io/golden-containers/ruby:3.0-bullseye 3.0/bullseye
-
+docker build --tag ghcr.io/golden-containers/ruby:3.0-slim-bullseye 3.0/slim-bullseye
 # Push
 
 docker push ghcr.io/golden-containers/ruby -a
