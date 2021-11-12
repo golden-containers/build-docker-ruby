92123ea185ea6c36d4e1f309579b6a586fe65a82
Jeremiah Gowdy <jeremiah@gowdy.me>
2021-11-12 09:19:31 -0800

    Use label flag

--- build.sh
+++ build.sh
@@ -13,15 +13,14 @@ cd ruby
 # Transform
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/buildpack-deps:bullseye/" 3.0/bullseye/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> 3.0/bullseye/Dockerfile
 
 sed -i -e "1 s/FROM.*/FROM ghcr.io\/golden-containers\/debian:bullseye-slim/; t" -e "1,// s//FROM ghcr.io\/golden-containers\/debian:bullseye-slim/" 3.0/slim-bullseye/Dockerfile
-echo "LABEL ${1:-DEBUG=TRUE}" >> 3.0/slim-bullseye/Dockerfile
 
 # Build
 
-docker build --tag ghcr.io/golden-containers/ruby:3.0-bullseye 3.0/bullseye
-docker build --tag ghcr.io/golden-containers/ruby:3.0-slim-bullseye 3.0/slim-bullseye
+docker build 3.0/bullseye/ --tag ghcr.io/golden-containers/ruby:3.0-bullseye --label ${1:-DEBUG=TRUE}
+docker build 3.0/slim-bullseye/ --tag ghcr.io/golden-containers/ruby:3.0-slim-bullseye --label ${1:-DEBUG=TRUE}
+
 # Push
 
 docker push ghcr.io/golden-containers/ruby -a
